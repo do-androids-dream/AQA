722793b0c992403bc5997f4844ec1cda77e1e401
Volodymyr Dukhanin <delta.dukhanin@gmail.com>
2023-12-25 04:29:33 +0100

    initial -tests done

---
+++ run_tests.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+
+echo "available commands:"
+echo "--printall"
+echo "--run --device <device_name> --test <test_name> (use --test before each <test_name>)"
+echo "--run all <device_name>"
+declare -a tests=(
+  "test_positive_addition"
+  "test_positive_substraction"
+  "test_negative_zerodivision"
+  "test_negative_tolarge"
+  )
+
+print_all() {
+  echo "Available tests:"
+  for test in "${tests[@]}"; do
+    echo "$test"
+  done
+  }
+
+run_test() {
+  echo "lets test..."
+  tests_to_run=()
+  if [ "$#" -eq 0 ]; then
+    echo "No test specified. Please provide test name"
+    print_all
+    exit 1
+  fi
+
+  while [ "$#" -gt 0 ]; do
+    case "$1" in
+      --device)
+        shift
+        device="$1"
+        ;;
+      --test)
+        shift
+        tests_to_run+=("$1")
+        ;;
+    esac
+    shift
+  done
+
+  if [[ -z "$device" ]]; then
+    echo "No device specified"
+    exit 1
+  fi
+
+  if [[ -z "$tests_to_run" ]]; then
+  echo "No test specified"
+  exit 1
+  fi
+
+  for test in "${tests_to_run[@]}"; do
+    if [[ " ${tests[@]} " =~ " $test " ]]; then
+      echo "Running test: $test on device: $device"
+      DEVICE="$device" python -m unittest tests.test_calc.CalculatorTests.$test
+    else
+      echo "Test '$test' not found."
+      print_all
+    fi
+  done
+}
+
+run_all() {
+  echo "lets test..."
+  if [ "$#" -eq 0 ]; then
+    echo "No device specified"
+    exit 1
+  fi
+
+  device="$2"
+  echo "Running all tests on device: $device"
+  DEVICE="$device" python -m unittest tests.test_calc
+}
+
+
+while [ "$#" -gt 0 ]; do
+  case "$1" in
+    --printall)
+      print_all
+      exit 0
+      ;;
+    --run)
+      shift
+      if [ "$1" == "all" ]; then
+        echo "all tests"
+        run_all "$@"
+        exit 0
+      fi
+
+      run_test "$@"
+      exit 0
+      ;;
+  esac
+done
---
+++ tests/test_calc.py
@@ -0,0 +1,96 @@
+import unittest
+from appium import webdriver
+from appium.options.android import UiAutomator2Options
+from appium.webdriver.common.appiumby import AppiumBy
+import os
+
+
+class CalculatorTests(unittest.TestCase):
+
+    def setUp(self):
+        device = os.environ.get('DEVICE')
+        capabilities = {
+            "platformName": "Android",
+            "automationName": "UiAutomator2",
+            "deviceName": device,
+            "appPackage": "com.google.android.apps.nexuslauncher",
+            "appActivity": ".Calculator",
+            "language": "en",
+            "locale": "US",
+            "noReset": True
+        }
+        appium_server_url = 'http://localhost:4723'
+        self.driver = webdriver.Remote(appium_server_url, options=UiAutomator2Options().load_capabilities(capabilities))
+        reset_result = '//android.widget.ImageButton[@content-desc="clear"]'
+        self.driver.find_element(by=AppiumBy.XPATH, value=reset_result).click()
+
+    def tearDown(self):
+        if self.driver:
+            self.driver.quit()
+
+    def test_positive_addition(self):
+        test_elements = [
+            '//android.widget.ImageButton[@content-desc="5"]',
+            '//android.widget.ImageButton[@content-desc="plus"]',
+            '//android.widget.ImageButton[@content-desc="3"]'
+        ]
+        result_preview = '//android.widget.TextView[@resource-id="com.google.android.calculator:id/result_preview"]'
+        expected = "8"
+
+        for element in test_elements:
+            self.driver.find_element(by=AppiumBy.XPATH, value=element).click()
+
+        result = self.driver.find_element(by=AppiumBy.XPATH, value = result_preview).text
+        self.assertEqual(result, expected)
+
+    def test_positive_substraction(self):
+        test_elements = [
+            '//android.widget.ImageButton[@content-desc="1"]',
+            '//android.widget.ImageButton[@content-desc="0"]',
+            '//android.widget.ImageButton[@content-desc="minus"]',
+            '//android.widget.ImageButton[@content-desc="5"]'
+        ]
+        result_preview = '//android.widget.TextView[@resource-id="com.google.android.calculator:id/result_preview"]'
+        expected = "5"
+
+        for element in test_elements:
+            self.driver.find_element(by=AppiumBy.XPATH, value=element).click()
+        result = self.driver.find_element(by=AppiumBy.XPATH, value = result_preview).text
+        self.assertEqual(result, expected)
+
+    def test_negative_zerodivision(self):
+        test_elements = [
+            '//android.widget.ImageButton[@content-desc="3"]',
+            '//android.widget.ImageButton[@content-desc="divide"]',
+            '//android.widget.ImageButton[@content-desc="0"]',
+            '//android.widget.ImageButton[@content-desc="equals"]'
+        ]
+        result_preview = '//android.widget.TextView[@resource-id="com.google.android.calculator:id/result_preview"]'
+        expected = "Can't divide by 0"
+
+        for element in test_elements:
+            self.driver.find_element(by=AppiumBy.XPATH, value=element).click()
+
+        result = self.driver.find_element(by=AppiumBy.XPATH, value=result_preview).text
+        self.assertEqual(result, expected)
+
+    def test_negative_tolarge(self):
+        element = '//android.widget.ImageButton[@content-desc="9"]'
+        operation = [
+            '//android.widget.ImageButton[@content-desc="power"]',
+            '//android.widget.ImageButton[@content-desc="equals"]'
+            ]
+        result_preview = '//android.widget.TextView[@resource-id="com.google.android.calculator:id/result_preview"]'
+        expected = "Value too large"
+
+        for i in range(2):
+            for _ in range(6):
+                self.driver.find_element(by=AppiumBy.XPATH, value=element).click()
+            self.driver.find_element(by=AppiumBy.XPATH, value=operation[i]).click()
+
+        result = self.driver.find_element(by=AppiumBy.XPATH, value=result_preview).text
+        self.assertEqual(result, expected)
+
+
+if __name__ == "main":
+    unittest.main()
